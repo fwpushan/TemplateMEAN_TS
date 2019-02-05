11bfcbc9def4e85ef1629868200c3f5348e0df7e
fwpushan <pushan@freshworks.io>
2019-02-05 09:40:12 -0800

    Data Model controllers

--- NodeTSApp/sources/server/core/dataModel.controller.ts
+++ NodeTSApp/sources/server/core/dataModel.controller.ts
@@ -12,8 +12,10 @@ export default class DataModelController<IModel extends IMongoModel> {
         return this.StoreModel as Model<IModel>
     }
 
+    // Fetching all modles
     async all(): Promise<any> {
         try {
+            //
             let models = await this.Model.find().exec()
             return models
         } catch (err) {
@@ -21,8 +23,10 @@ export default class DataModelController<IModel extends IMongoModel> {
         }
     }
 
+    // Finding and fetching model
     async find(inputParam: any): Promise<any> {
         try {
+            // Finding object/ Document in db
             let models = await this.Model.find(inputParam).exec()
             return models
         } catch (err) {
@@ -30,10 +34,30 @@ export default class DataModelController<IModel extends IMongoModel> {
         }
     }
 
+    // Creating 
     async create(input: IModel): Promise<any> {
         try {
+            // Creating New model object in db
             let models = new this.Model(input)
             return await models.save()
+        } catch (err) {
+            throw err
+        }
+    }
+
+    async createUniqe(input: IModel): Promise<any> {
+        try {
+            // Find the existing model object
+            let model = await this.Model.findOne(input).exec()
+            if (model) {
+                // Returning Existing Model
+                return model
+            } else {
+                // Creating New model object in db
+                let models = new this.Model(input)
+                return await models.save()
+            }
+
         } catch (err) {
             throw err
         }
